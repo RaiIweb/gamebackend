c9782629d6c6e9d2d20d8b050472969a8c44a042
RaiIweb <rai.iwebcode@gmail.com>
2021-11-13 12:25:13 +0530

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
       })
 
       await newRecord.save()
-      res.cookie(`login`, newId, {httpOnly: false, secure: false, sameSite: "none"})
+      res.cookie(`login`, newId, {httpOnly: false, secure: true, sameSite: "none"})
       return res.json({ login: true })
 
     } else {
