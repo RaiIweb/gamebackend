22bff0911dac35bbf3b4d24fb0b2007656a147fb
RaiIweb <rai.iwebcode@gmail.com>
2021-11-13 19:09:47 +0530

    timings

--- models/userLogin.js
+++ models/userLogin.js
@@ -4,6 +4,11 @@ var userLogins = mongoose.model('userLogin',
 {
     email : {type:String , unique:true ,required: true} ,
     cookie : {type:String },
+    expireAt : {
+        type: Date,
+        default: Date.now,
+        index: { expires: '2m' }, 
+    }
         
 },'userLogin')
 
